863fbddeb561924df3674b58407f15eb0311868d
nqd182 <nguyendung180203@gmail.com>
2024-11-05 17:45:20 +0700

    xoa node modules khoi git

--- helper/pagination.js
+++ helper/pagination.js
@@ -1,11 +1,11 @@
 module.exports = (objectPagination,query,countProducts) =>{
     if(query.page){
-        objectPagination.currentPage = parseInt(query.page) // trang hien tai dang truy cap
+        objectPagination.currentPage = parseInt(query.page) // lay trang hien tai dang truy cap tu query
     }
-    objectPagination.skip = (objectPagination.currentPage-1)*objectPagination.limitItems // ap dung cong thuc 
+    objectPagination.skip = (objectPagination.currentPage-1)*objectPagination.limitItems // ap dung cong thuc de tinh so trang can skip 
 
         //Tinh so trang can de phan trang
-    const totalPage = Math.ceil(countProducts/objectPagination.limitItems); //  tinh tong so trang
+    const totalPage = Math.ceil(countProducts/objectPagination.limitItems); //  tinh tong so trang 
     objectPagination.totalPage = totalPage;
     return objectPagination
 }
