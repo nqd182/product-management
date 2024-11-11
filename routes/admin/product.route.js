b55d23ccf860bd1b3447d89295cb87eb71711c39
nqd182 <nguyendung180203@gmail.com>
2024-11-11 14:43:24 +0700

    cloudinary upload images

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,23 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const multer = require('multer') // thu vien de upload file
-const cloudinary = require('cloudinary').v2
-const streamifier = require('streamifier')
+
 //const storageMulter = require("../../helper/storageMulter") // import ham dinh nghia cach file duoc luu
 //const upload = multer({ dest: './public/uploads/' }) // dest: đường dẫn lưu ảnh, dest sẽ đứng từ thư mục gốc
 
 
 const upload = multer() //{storage: storageMulter()}tùy chọn storage để tùy chỉnh cách multer lưu trữ tệp như đổi tên file và nơi lưu
-// Cloudinary
-cloudinary.config({ 
-    cloud_name: 'dlfj1mhdc', 
-    api_key: '178377237317347', 
-    api_secret: '741-vPnJS4Ij4e7v4aC7Briyl90' // Click 'View API Keys' above to copy your API secret
-});
-//End cloudinary
+
 
 const controller = require("../../controller/admin/product.controller")
 const validate = require("../../validates/admin/product.validate")
+
+const uploadCloud = require("../../middleware/admin/uploadCloud.middleware")
 
  router.get('/', controller.index)
  router.patch('/change-status/:status/:id', controller.changeStatus) // :router dong
@@ -27,34 +22,7 @@ const validate = require("../../validates/admin/product.validate")
  router.post(
     '/create',
     upload.single('thumbnail'),
-    function (req, res, next) { // hàm để up load ảnh lên database
-        if(req.file){
-            let streamUpload = (req) => {
-                return new Promise((resolve, reject) => {
-                    let stream = cloudinary.uploader.upload_stream(
-                      (error, result) => {
-                        if (result) {
-                          resolve(result);
-                        } else {
-                          reject(error);
-                        }
-                      }
-                    );
-        
-                  streamifier.createReadStream(req.file.buffer).pipe(stream);
-                });
-            };
-        
-            async function upload(req) {
-                let result = await streamUpload(req);
-                req.body[req.file.fieldname] = result.secure_url //secure_url là link ảnh online, 
-                //req.file.fieldname là tên của input người dùng đã upload .
-                next()
-            }
-        upload(req);
-
-        }else next()
-    },
+    uploadCloud.upload,
     validate.createPost, 
     controller.createPost
     ) // route trung nhau dc nhung pth phai khac
