d8e442ff0c180e3f14a11c656f44df64d708e2d3
nqd182 <nguyendung180203@gmail.com>
2024-11-20 19:23:39 +0700

    chinh sua nhom quyen

--- controller/admin/role.controller.js
+++ controller/admin/role.controller.js
@@ -30,3 +30,32 @@ module.exports.createPost= async (req, res) => {
     res.redirect(`${systemConfig.prefixAdmin}/roles`)
 }
 
+// [GET] /admin/roles/edit/:id
+module.exports.edit= async (req, res) => { 
+try {
+    let find = {
+        _id: req.params.id,
+        deleted: false
+    }
+    let record = await Role.findOne(find)
+
+    res.render("admin/pages/roles/edit",{
+        pageTitle: "Chỉnh sửa nhóm quyền",
+        record: record
+    })
+} catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/roles`)
+}
+   
+}
+// [GET] /admin/roles/edit/:id
+module.exports.editPatch= async (req, res) => { 
+    
+    await Role.updateOne({_id: req.params.id},req.body)
+
+    res.redirect(`${systemConfig.prefixAdmin}/roles`)
+
+    res.render("admin/pages/roles/edit",{
+        pageTitle: "Chỉnh sửa nhóm quyền",
+    })
+}
